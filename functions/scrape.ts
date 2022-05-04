9771cb189c603b795f0e747c4e2841863f78ad9a
Eric Kelley <hello@ericmkelley.com>
2022-05-04 12:16:06 -0400

    optimize: puppeteer optimizations

--- functions/scrape.ts
+++ functions/scrape.ts
@@ -1,8 +1,44 @@
 import { Handler } from "@netlify/functions";
 import chromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
-import cheerio from "cheerio";
-
+import * as cheerio from "cheerio";
+const minimal_args = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+];
 const handler: Handler = async (event) => {
   const { query } = event.queryStringParameters;
   const ID = await scrapeResults(query);
@@ -17,7 +53,7 @@ const scrapeResults = async (query: string) => {
   const fullQuery = ytQueryBase + query.replace(" ", "+") + " official video";
 
   const browser = await puppeteer.launch({
-    args: ["--no-zygote", "--no-sandbox"],
+    args: minimal_args,
     executablePath: process.env.CHROME_EXE || (await chromium.executablePath),
     headless: true,
   });
