69e4d569f40693a6cfcc1f7155ee7af457b798d1
Eric Kelley <hello@ericmkelley.com>
2022-05-14 01:05:11 -0400

    the never ending journey of stopping zombie chrome processes

--- functions/scrape.ts
+++ functions/scrape.ts
@@ -1,6 +1,8 @@
 import { Handler } from "@netlify/functions";
 import chromium from "chrome-aws-lambda";
 import * as cheerio from "cheerio";
+import kill from "tree-kill";
+
 const handler: Handler = async (event) => {
   const { query } = event.queryStringParameters;
   const ID = await scrapeResults(query);
@@ -10,12 +12,84 @@ const handler: Handler = async (event) => {
   };
 };
 
+const nuclear_args = [
+  "--disable-2d-canvas-clip-aa",
+  "--disable-2d-canvas-image-chromium",
+  "--disable-3d-apis",
+  "--disable-accelerated-2d-canvas",
+  "--disable-accelerated-jpeg-decoding",
+  "--disable-accelerated-mjpeg-decode",
+  "--disable-accelerated-video-decode",
+  "--disable-app-list-dismiss-on-blur",
+  "--disable-audio-output",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-canvas-aa",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-extensions-with-background-pages",
+  "--disable-composited-antialiasing",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-extensions",
+  "--disable-features=TranslateUI,BlinkGenPropertyTrees",
+  "--disable-field-trial-config",
+  "--disable-fine-grained-time-zone-detection",
+  "--disable-geolocation",
+  "--disable-gl-extensions",
+  "--disable-gpu",
+  "--disable-gpu-early-init",
+  "--disable-gpu-sandbox",
+  "--disable-gpu-watchdog",
+  "--disable-histogram-customizer",
+  "--disable-in-process-stack-traces",
+  "--disable-infobars",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-renderer-backgrounding",
+  "--disable-session-crashed-bubble",
+  "--disable-setuid-sandbox",
+  "--disable-site-isolation-trials",
+  "--disable-software-rasterizer",
+  "--disable-sync",
+  "--disable-threaded-animation",
+  "--disable-threaded-scrolling",
+  "--disable-translate",
+  "--disable-webgl",
+  "--disable-webgl2",
+  "--enable-features=NetworkService",
+  "--force-color-profile=srgb",
+  "--headless",
+  "--hide-scrollbars",
+  "--ignore-certifcate-errors",
+  "--ignore-certifcate-errors-spki-list",
+  "--ignore-certificate-errors",
+  "--ignore-certificate-errors-spki-list",
+  "--ignore-gpu-blacklist",
+  "--ignore-ssl-errors",
+  "--log-level=3",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-crash-upload",
+  "--no-default-browser-check",
+  "--no-experiments",
+  "--no-first-run",
+  "--no-sandbox",
+  "--no-zygote",
+  "--renderer-process-limit=1",
+  "--safebrowsing-disable-auto-update",
+  "--silent-debugger-extension-api",
+  "--single-process",
+  "--unhandled-rejections=strict",
+  "--window-position=0,0",
+];
+
 const scrapeResults = async (query: string) => {
   const ytQueryBase = "https://www.youtube.com/results?search_query=";
   const fullQuery = `${ytQueryBase}${query.replace(" ", "+")} official video`;
 
   const browser = await chromium.puppeteer.launch({
-    args: [...chromium.args, "--no-zygote"],
+    args: [...chromium.args, ...nuclear_args],
     executablePath: process.env.CHROME_EXE || (await chromium.executablePath),
     headless: true,
   });
@@ -34,7 +108,10 @@ const scrapeResults = async (query: string) => {
   const html = await page.content();
   const finalID = parse(html);
 
+  const browser_pid = await browser.process().pid;
   await browser.close();
+  kill(browser_pid, "SIGKILL");
+
   return finalID;
 };
 
--- src/App.vue
+++ src/App.vue
@@ -446,14 +446,44 @@ const reset = (): void => {
                 class="2xl:w-1/5 xl:w-1/4 lg:w-1/3 md:1/2 w-full p-2 py-4 flex flex-row blur-50"
               >
                 <div
-                  class="aspect-square w-full h-full rounded-md flex items-end bg-cover"
+                  class="aspect-square w-full h-full rounded-md flex flex-col justify-between bg-cover overflow-hidden"
                   :style="{
                   backgroundImage:
                     'url(' + track.track.album.images![0].url + ')',
                 }"
                 >
+                  <div class="flex flex-row items-end">
+                    <div
+                      class="h-10 w-10 bg-slate-900/70 backdrop-blur-md rounded-br-lg shadow-md flex items-center justify-center"
+                    >
+                      <div>
+                        <span
+                          v-if="
+                            finalTracks.some(
+                              (e) =>
+                                e.spotify === track.track.external_urls.spotify
+                            )
+                          "
+                          class="text-emerald-400"
+                        >
+                          <i class="fas fa-circle-check" />
+                        </span>
+                        <span
+                          v-else-if="
+                            hasTimedOut(track.track.external_urls.spotify)
+                          "
+                          class="text-red-400"
+                        >
+                          <i class="fas fa-circle-x" />
+                        </span>
+                        <span v-else class="text-slate-100">
+                          <i class="fas fa-compact-disc fa-spin" />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
                   <div
-                    class="p-4 bg-slate-900/70 backdrop-blur-md border-slate-600 shadow-lg text-white w-full flex flex-col rounded-b-md"
+                    class="p-4 bg-slate-900/70 backdrop-blur-md border-slate-600 shadow-lg text-white w-full flex flex-col rounded-b-md -mb-10 hover:mb-0 transition-all ease-in-out delay-100"
                   >
                     <p class="mb-1 font-bold truncate">
                       {{ track.track.name }}
@@ -492,17 +522,10 @@ const reset = (): void => {
                         v-else-if="
                           hasTimedOut(track.track.external_urls.spotify)
                         "
-                        class="border border-red-400 rounded text-slate-300 hover:text-slate-50 py-1 px-2 font-bold bg-opacity-50 hover:bg-opacity-100 text-xs"
+                        class="border border-red-400 rounded text-slate-300 py-1 px-2 font-bold bg-opacity-50 text-xs select-none"
                       >
                         <i class="fas fa-circle-x mr-2" />
                         Timed Out
-                      </a>
-                      <a
-                        v-else
-                        class="bg-transparent rounded text-slate-300 hover:text-slate-50 py-1 px-2 font-bold bg-opacity-50 hover:bg-opacity-100 text-xs"
-                      >
-                        <i class="fas fa-compact-disc fa-spin mr-2" />
-                        Scraping...
                       </a>
                     </div>
                   </div>
